7cf0b1cb11cee6c1f677ab65ffc031da4875c066
Omar Saeed <omar.ahmed.saeed.1999@gmail.com>
2023-06-01 06:05:19 +0300

    add: use javascript for mobile navigation button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,12 @@ const yearEl = document.querySelector(".footer-year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
